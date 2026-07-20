a174ee4be0b7ea1eab98ef6f181d06829deb2df7
TYDTU <TYDTU@users.noreply.github.com>
2026-07-20 00:26:02 -0400

    Update blue box Room Refresh description to 'refresh their space'

--- script.js
+++ script.js
@@ -278,7 +278,7 @@ const packageData = {
     productId: "refresh",
     title: "Room Refresh",
     copy:
-      "Dorms can stink--theirs doesn't have to. Banish the funk and refresh your space so they can enjoy breathing deeply again.",
+      "Dorms can stink--theirs doesn't have to. Banish the funk and refresh their space so they can enjoy breathing deeply again.",
   },
   "First-year starter|Snack attack": {
     productId: "snack",
